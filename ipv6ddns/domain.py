16e4c87edee9d8c52953b0831e29c142d5a16f99
skidmarkturbo <skidmarkturbo@pm.me>
2023-06-20 03:58:32 +0530

    adds context parsing from argparse

--- ipv6ddns/domain.py
+++ ipv6ddns/domain.py
@@ -65,56 +65,9 @@ class FirewallEntry:
 
 
 # pylint: disable=locally-disabled, too-few-public-methods,
-class CommonContext:
-    """Context common to all operations
+class ValidationError:
+    """Container struct for validation errors
     """
-
-    def __init__(self) -> None:
-        self.assume_yes = False
-        self.dry_run = False
-        self.args = None
-
-
-# pylint: disable=locally-disabled, too-few-public-methods,
-class DNSContext:
-    """Context specific to DNS operations
-    """
-
-    def __init__(self) -> None:
-        self.plugin = None
-        self.fqdns = []
-
-
-# pylint: disable=locally-disabled, too-few-public-methods,
-class FirewallContext:
-    """Context specific to Firewall operations
-    """
-
-    def __init__(self) -> None:
-        self.plugin = None
-        self.tcp_ports = []
-        self.udp_ports = []
-        self.host_id = None
-
-
-# pylint: disable=locally-disabled, too-few-public-methods,
-class ResolverContext:
-    """Context specific to IPV6 resolution operations
-    """
-
-    def __init__(self) -> None:
-        self.plugin = None
-
-
-# pylint: disable=locally-disabled, too-few-public-methods
-class DDNSContext:
-    """Execution context of DDNS oprations. Holds all the configuration
-    and arguments (eg. FQDNs, plugins to use, and plugin configuration).
-    The DDNS workflow executes over a context.
-    """
-
-    def __init__(self) -> None:
-        self.common = CommonContext()
-        self.dns = DNSContext()
-        self.firewall = FirewallContext()
-        self.ipv6 = ResolverContext()
+    def __init__(self, plugin_name: str, message: str) -> None:
+        self.plugin_name = plugin_name
+        self.message = message
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,42 @@
 """
 Tests for CLI
 """
+import pytest
+from ipv6ddns.cli import Cli
+
+
+def test_cli_help():
+    """Tests calling help doesn't crash
+    """
+    cli = Cli(cli_args=["--help"])
+
+    with pytest.raises(SystemExit) as sys_exit:
+        cli.execute()
+    assert sys_exit.type == SystemExit
+    assert sys_exit.value.code == 0
+
+    cli = Cli(cli_args=["-h"])
+
+    with pytest.raises(SystemExit) as sys_exit:
+        cli.execute()
+    assert sys_exit.type == SystemExit
+    assert sys_exit.value.code == 0
+
+
+def test_dry_run_and_force_together():
+    """Test that using --dry-run and --force together
+    raises error
+    """
+    cli = Cli(cli_args=["--dry-run", "--force"])
+
+    with pytest.raises(SystemExit) as sys_exit:
+        cli.execute()
+    assert sys_exit.type == SystemExit
+    assert sys_exit.value.code == 3
+
+    cli = Cli(cli_args=["--dry-run", "-f"])
+
+    with pytest.raises(SystemExit) as sys_exit:
+        cli.execute()
+    assert sys_exit.type == SystemExit
+    assert sys_exit.value.code == 3
